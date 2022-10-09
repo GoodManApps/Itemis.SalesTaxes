517731649d46ea5af2f16a6038fd89d97aa9ba88
Vyacheslav Shengur <slavcho21@gmail.com>
2022-10-09 22:15:59 +0300

    Add IQualifier interface for additional processing of elements

---
+++ Itemis.SalesTaxes.Abstraction/TaxesCalculator/Qualifiers/IQualifier.cs
@@ -0,0 +1,18 @@
+﻿using Itemis.SalesTaxes.Domain.Interfaces;
+
+namespace Itemis.SalesTaxes.Abstraction.TaxesCalculator.Qualifiers
+{
+    /// <summary>
+    /// The qualifier interface for the element.
+    /// </summary>
+    /// <typeparam name="T">The element type to be qualified.</typeparam>
+    public interface IQualifier<T>
+        where T : IItem
+    {
+        /// <summary>
+        /// Perform item qualification.
+        /// </summary>
+        /// <param name="item">Element for qualification.</param>
+        void Qualify(T item);
+    }
+}
