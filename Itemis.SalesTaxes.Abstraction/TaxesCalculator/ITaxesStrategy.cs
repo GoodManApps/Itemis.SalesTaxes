acdfa455e57ffb7ea756c2c2f63db7f9d96471e6
Vyacheslav Shengur <slavcho21@gmail.com>
2022-10-09 21:24:04 +0300

    Add abstraction types for check and calculate taxes

---
+++ Itemis.SalesTaxes.Abstraction/TaxesCalculator/ITaxesStrategy.cs
@@ -0,0 +1,18 @@
+﻿namespace Itemis.SalesTaxes.Abstraction.TaxesCalculator
+{
+    /// <summary>
+    /// Tax calculation strategy
+    /// </summary>
+    /// <typeparam name="TIn">The input parameter for which you want to calculate the tax</typeparam>
+    /// <typeparam name="TOut">Output with calculated tax</typeparam>
+    public interface ITaxesStrategy<TIn, TOut>
+    {
+        /// <summary>
+        /// Realize the process to transform input data 
+        /// to output format with tax calculation
+        /// </summary>
+        /// <param name="income">Incoming data</param>
+        /// <returns></returns>
+        TOut EvaluateTaxes(TIn income);
+    }
+}
---
+++ Itemis.SalesTaxes.Abstraction/TaxesChecker/ITaxesChecker.cs
@@ -0,0 +1,18 @@
+﻿using Itemis.SalesTaxes.Domain.Models.Taxes;
+
+namespace Itemis.SalesTaxes.Abstraction.TaxesChecker
+{
+    /// <summary>
+    /// The interface for implementing the tax check for the element.
+    /// </summary>
+    /// <typeparam name="T">Item Type</typeparam>
+    public interface ITaxesChecker<T>
+    {
+        /// <summary>
+        /// Set tax types
+        /// </summary>
+        /// <param name="el">The element for which to check taxes.</param>
+        /// <returns>Collection of applied taxes for an element</returns>
+        IEnumerable<BaseTax> SetTaxes(T el);
+    }
+}
