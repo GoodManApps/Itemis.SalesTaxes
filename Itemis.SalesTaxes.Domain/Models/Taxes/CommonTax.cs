5b1725fd5f4948f69b31df1cc3c8edeeb325e239
Vyacheslav Shengur <slavcho21@gmail.com>
2022-10-09 16:48:18 +0300

    refactoring: delete unused usings

--- Itemis.SalesTaxes.Domain/Models/Taxes/CommonTax.cs
+++ Itemis.SalesTaxes.Domain/Models/Taxes/CommonTax.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Itemis.SalesTaxes.Domain.Models.Taxes
+﻿namespace Itemis.SalesTaxes.Domain.Models.Taxes
 {
     /// <summary>
     /// Common tax for some region
