bd845972a7675eac5fa87a799bac3389309f2d0b
Vyacheslav Shengur <slavcho21@gmail.com>
2022-10-10 16:38:47 +0300

    Added app configuration (without using right now) and console text configuration

---
+++ Itemis.SalesTaxes/Settings/ProductTaxSettings.cs
@@ -0,0 +1,20 @@
+﻿using Itemis.SalesTaxes.Domain.Enums;
+
+namespace Itemis.SalesTaxes.Settings
+{
+    /// <summary>
+    /// Settings for product tax calculation
+    /// </summary>
+    public class ProductTaxSettings
+    {
+        /// <summary>
+        /// Categories that are exempt from specific taxes
+        /// </summary>
+        public Dictionary<string, List<ProductCategory>> ExemptForTaxes { get; set; }
+
+        /// <summary>
+        /// Dictionary of keywords to define the product category
+        /// </summary>
+        public Dictionary<ProductCategory, HashSet<string>> CategoriesKeywords { get; set; }
+    }
+}
